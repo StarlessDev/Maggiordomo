129f54f76fa3e781af7dae1cea6cef7b961fbf95
StarlessDev <contact@starless.dev>
2023-07-19 13:18:58 +0200

    Fixed Matcher#getMemberFromInput compatibility with new handles

--- src/main/java/dev/starless/maggiordomo/utils/Matcher.java
+++ src/main/java/dev/starless/maggiordomo/utils/Matcher.java
@@ -13,30 +13,34 @@ public class Matcher {
     // The USERNAME_TAG pattern is taken directly from JDA source code, credits to them
     // @ https://github.com/DV8FromTheWorld/JDA/blob/master/src/main/java/net/dv8tion/jda/api/entities/User.java
 
-    private static final Pattern USERNAME_TAG = Pattern.compile("(.{2,32})#(\\d{4})");
-    private static final Pattern ID = Pattern.compile("\\d{18,20}");
+    private final Pattern USERNAME_TAG = Pattern.compile("(.{2,32})#(\\d{4})");
+    private final Pattern ID = Pattern.compile("\\d{18,20}");
+    private final Pattern HANDLE = Pattern.compile("^@?(\\w|\\.){2,32}$");
+
+    public Optional<Member> getMemberFromInput(Guild guild, String input) {
+        Member member = null;
+        input = input.replaceAll(" ", "");
+
+        if (isID(input)) {
+            member = guild.getMemberById(input);
+        } else if (isFullUsername(input)) {
+            member = guild.getMemberByTag(input);
+        } else if(isHandle(input)) {
+            member = guild.getMemberByTag(input.replace("@", "").concat("#0000"));
+        }
+
+        return Optional.ofNullable(member);
+    }
 
     private boolean isFullUsername(String string) {
         return USERNAME_TAG.matcher(string).matches();
     }
 
     private boolean isID(String string) {
-        return ID.matcher(string)
-                .results()
-                .findAny()
-                .isPresent();
+        return ID.matcher(string).matches();
     }
 
-    public Optional<Member> getMemberFromInput(Guild guild, String input) {
-        Member member = null;
-        input = input.trim();
-
-        if (Matcher.isFullUsername(input)) {
-            member = guild.getMemberByTag(input);
-        } else if (input.startsWith("@") || Matcher.isID(input)) {
-            member = guild.getMemberById(input);
-        }
-
-        return Optional.ofNullable(member);
+    private boolean isHandle(String string) {
+        return HANDLE.matcher(string).matches();
     }
 }
