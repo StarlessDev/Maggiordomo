68065ea6ec73d3b73722db8df4daeb0c21b51c32
StarlessDev <contact@starless.dev>
2023-07-23 18:52:51 +0200

    Version bump (pt. 2)

--- src/main/java/dev/starless/maggiordomo/Main.java
+++ src/main/java/dev/starless/maggiordomo/Main.java
@@ -18,7 +18,7 @@ import java.time.Duration;
 
 public class Main {
 
-    @Getter private static final String version = "2.0.2";
+    @Getter private static final String version = "2.0.3";
 
     public static void main(String[] args) {
         BotLogger.info("Running Maggiordomo v%s by Starless", version);
